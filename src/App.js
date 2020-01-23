8160f232c480e899f9ee5231a16da7627dd8a389
Douglas Santos <douglas_motasts@outlook.com>
2020-01-23 14:48:49 -0300

    Adicionando estilização e novas configs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
+import GlobalStyle from './style/global';
 
 import Routes from './routes';
 import history from './services/history';
@@ -10,6 +11,7 @@ function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate = false,
@@ -14,7 +17,18 @@ export default function RouteWrapper({
   } else if (signed && !isPrivate) {
     return <Redirect to="/dashboard" />;
   } else {
-    return <Route {...rest} component={Component} />;
+    const Layout = signed ? DefaultLayout : AuthLayout;
+
+    return (
+      <Route
+        {...rest}
+        render={props => (
+          <Layout>
+            <Component {...props} />
+          </Layout>
+        )}
+      />
+    );
   }
 }
 

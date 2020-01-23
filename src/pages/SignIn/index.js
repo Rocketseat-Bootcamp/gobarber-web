f657d56eb49ce7e5a4b6461ac11e24927ca965b7
Douglas Santos <douglas_motasts@outlook.com>
2020-01-23 15:31:53 -0300

    Adicionando a validacao na autenticacao

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '~/assets/logo.svg';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatorio'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -11,7 +19,7 @@ export default function SignIn() {
   return (
     <>
       <img src={logo} alt="logo" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
 

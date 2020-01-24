54dd0ddaf4419d9d836923728fd0a92175a34e79
Douglas Santos <douglas_motasts@outlook.com>
2020-01-24 16:25:32 -0300

    Adicioando o Header no ambiente logado.

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile } from './styles';
+import logoHeader from '~/assets/logo_header.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logoHeader} alt="" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>DOUGLAS SANTOS</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="profile"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}

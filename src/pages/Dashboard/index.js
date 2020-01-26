507e13f58ffd983b6e153c5c8c546a37f8733344
Douglas Santos <douglas_motasts@outlook.com>
2020-01-26 11:53:06 -0300

    Ajustes da data

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,56 @@
-import React from 'react';
-import api from '~/services/api';
-// import { Container } from './styles';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Jessica Cristine</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Jessica Cristine</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Jessica Cristine</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
